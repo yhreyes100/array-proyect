525adc34ca36f9337838b7172209045ba846721e
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-08-17 02:10:18 +0000

    comm

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".net", ".com", ".us", ".io", ".gov"];
+  for (let ii of pronoun)
+    for (let jj of adj)
+      for (let kk of noun)
+        for (let ll of dom)
+          document.getElementById("list").innerHTML +=
+            "<li>" + ii + jj + kk + ll + "</li>";
 };
